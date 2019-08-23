9a433f61213a0c89a737eb1d2647ba56e3064e25
henrik.nordlie <henriknordlie99@gmail.com>
2019-08-23 13:51:48 +0200

    23.08 versjon

---
+++ python/raspi/Mazey.py
@@ -0,0 +1,182 @@
+# Example code for IMRT100 robot project
+
+
+# Import some modules that we need
+import imrt_robot_serial
+import signal
+import time
+import sys
+import random
+
+LEFT = -1
+RIGHT = 1
+FORWARDS = 1
+BACKWARDS = -1
+DRIVING_SPEED = 150
+TURNING_SPEED = 150
+TURN_DISTANCE = 15
+STOP_DISTANCE = 5
+
+def stop_robot(duration):
+
+    iterations = int(duration * 10)
+    
+    for i in range(iterations):
+        motor_serial.send_command(0, 0)
+        time.sleep(0.10)
+
+
+
+def drive_robot(direction, duration):
+    
+    speed = DRIVING_SPEED * direction
+    iterations = int(duration * 10)
+
+    for i in range(iterations):
+        motor_serial.send_command(speed, speed)
+        time.sleep(0.10)
+
+
+def turn_robot_right():
+
+    direction = 1
+    iterations = random.randint(9, 9)
+    
+    for i in range(iterations):
+        motor_serial.send_command(TURNING_SPEED * direction, -TURNING_SPEED * direction)
+        time.sleep(0.10)
+
+def turn_robot_left():
+
+    direction = -1
+    iterations = random.randint(9, 9)
+    
+    for i in range(iterations):
+        motor_serial.send_command(TURNING_SPEED * direction, -TURNING_SPEED * direction)
+        time.sleep(0.10)
+
+def turn_robot_slight_left():
+
+    direction = -1
+    iterations = (1)
+    
+    for i in range(iterations):
+        motor_serial.send_command(TURNING_SPEED * direction, -TURNING_SPEED * direction)
+        time.sleep(0.10)
+
+def turn_robot_slight_right():
+
+    direction = 1
+    iterations = (1)
+    
+    for i in range(iterations):
+        motor_serial.send_command(TURNING_SPEED * direction, -TURNING_SPEED * direction)
+        time.sleep(0.05)
+        
+        
+# We want our program to send commands at 10 Hz (10 commands per second)
+execution_frequency = 10 #Hz
+execution_period = 1. / execution_frequency #seconds
+
+
+# Create motor serial object
+motor_serial = imrt_robot_serial.IMRTRobotSerial()
+
+
+# Open serial port. Exit if serial port cannot be opened
+try:
+    motor_serial.connect("/dev/ttyACM0")
+except:
+    print("Could not open port. Is your robot connected?\nExiting program")
+    sys.exit()
+
+    
+# Start serial receive thread
+motor_serial.run()
+
+
+# Now we will enter a loop that will keep looping until the program terminates
+# The motor_serial object will inform us when it's time to exit the program
+# (say if the program is terminated by the user)
+print("Entering loop. Ctrl+c to terminate")
+while not motor_serial.shutdown_now :
+
+
+    ###############################################################
+    # This is the start of our loop. Your code goes below.        #
+    #                                                             #
+    # An example is provided to give you a starting point         #
+    # In this example we get the distance readings from each of   #
+    # the two distance sensors. Then we multiply each reading     #
+    # with a constant gain and use the two resulting numbers      #
+    # as commands for each of the two motors.                     #
+    #  ________________________________________________________   #
+    # |                                                        |  #
+    # V                                                           #
+    # V                                                           #
+    ###############################################################
+
+
+
+
+
+
+    # Get and print readings from distance sensors
+    dist_1 = motor_serial.get_dist_1()
+    dist_2 = motor_serial.get_dist_2()
+    dist_3 = motor_serial.get_dist_3()
+    dist_4 = motor_serial.get_dist_4()
+    print("Dist 1:", dist_1, "   Dist 2:", dist_2, "  Dist 3:", dist_3, "   Dist 4:", dist_4)
+
+    # Check if there is an obstacle in the way
+
+    if dist_1 < TURN_DISTANCE or dist_2 < TURN_DISTANCE or dist_3 < TURN_DISTANCE or dist_4 < TURN_DISTANCE:
+        # There is an obstacle in front of the robot
+        # Turn slightly to the side with more room
+        print("Correcting!")
+        if dist_1 < TURN_DISTANCE:
+            turn_robot_slight_right()
+        elif dist_4 < TURN_DISTANCE:
+            turn_robot_slight_left()
+        else:
+            drive_robot(BACKWARDS, 0.1)
+            if dist_1 > dist_4:
+                turn_robot_slight_right()
+            else:
+                turn_robot_slight_left()
+
+    #elif  dist_2 < STOP_DISTANCE or dist_3 < STOP_DISTANCE:
+    #    drive_robot(BACKWARDS, 0.1)
+    #    if dist_1 > dist_4:
+    #        turn_robot_slight_left()
+    #   else:
+    #        turn_robot_slight_right()
+    
+    else:
+        # If there is nothing in front of the robot it continus driving forwards
+        drive_robot(FORWARDS, 0.1)
+
+
+        
+                
+
+
+
+    ###############################################################
+    #                                                           A #
+    #                                                           A #
+    # |_________________________________________________________| #
+    #                                                             #
+    # This is the end of our loop,                                #
+    # execution continus at the start of our loop                 #
+    ###############################################################
+    ###############################################################
+
+
+
+
+
+# motor_serial has told us that its time to exit
+# we have now exited the loop
+# It's only polite to say goodbye
+print("Goodbye")
--- python/raspi/lys.py
+++ python/raspi/lys.py
@@ -2,16 +2,28 @@ import RPi.GPIO as GPIO
 import time
 
 LED_PIN = 2
+BUTTON_PIN = 4
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+
+
+led_on = False
+GPIO.setup(LED_PIN, led_on)
+
+prev_button_pressed = GPIO.input(BUTTON_PIN)
+
 
 while True:
-    GPIO.output(LED_PIN, True)
-    print("LED on")
-    time.sleep(0.4)
+    current_button_pressed = GPIO.input(BUTTON_PIN)
 
-    GPIO.output(LED_PIN, False)
-    print("LED off")
-    time.sleep(0.4)
+    if current_button_pressed and not prev_button_pressed:
+        led_on = not led_on
+        GPIO.output(LED_PIN, led_on)
+        print("LED on:", led_on)
+
+    prev_button_pressed = current_button_pressed
+    time.sleep(0.05)
